6ee7cb1e466bcfb761eac2556d82efed188cf3a4
Kumaraguru <gurugitacc@gmail.com>
2023-06-23 08:59:43 +0530

    updated

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -21,4 +21,44 @@ class InvoiceController extends Controller
         $products = Product::get();
         return view('invoice.add')->with(['products'=>$products]);
     }
+
+    public function store(Request $request)
+    {
+        dd($request->all());
+        $request->validate([
+            'customer_name' => 'required',
+            'phone' => 'required',
+            'company_name' => 'required',
+            'product_id' => 'required',
+            'rate' => 'required',
+            'qty' => 'required',
+        ]);
+
+        $grand_amount = 
+
+        $invoice = new Invoice();
+
+        $invoice->customer_name = $request->customer_name;
+        $invoice->phone = $request->phone;
+        $invoice->company_name = $request->company_name;
+        $invoice->grand_amount = $grand_amount;
+        $invoice->bike_color = $request->bike_color;
+        $invoice->bike_model = $request->bike_model;
+        $invoice->service_options = json_encode($request->services);
+        $invoice->note = $request->note;
+        $invoice->save();
+        
+        // $services = Service::whereIn('id',$request->services)->get()->toArray();
+        // $mailData = [
+        //     "name" => auth()->user()->name,
+        //     "service_date" => $request->service_date,
+        //     "service_details" => $services,
+        //     "note"=>$request->note
+        // ];
+        // $admin = User::where('id',$request->service_center_id)->first();
+        // Mail::to($admin->email)->send(new AdminMail($mailData));
+
+        return redirect()->route('invoivces')
+                        ->with('success','Booking created successfully.');
+    }
 }
--- resources/views/invoice/add.blade.php
+++ resources/views/invoice/add.blade.php
@@ -4,7 +4,7 @@
 <div class="row">
     <div class="col-lg-12" style="display: flex; justify-content: space-between;">
         <div class="pull-left">
-            <h2>Add New Product</h2>
+            <h2>Add New Invoice</h2>
         </div>
         <div class="pull-right">
             <a class="btn btn-primary" href="{{ route('products.index') }}"> Back</a>
@@ -35,48 +35,62 @@
             </div>
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
+                    <strong>Customer Phone:</strong>
+                    <input type="text" name="phone" value="" pattern="[0-9]*" class="form-control phone" placeholder="Customer Phone">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
                     <strong>Company Name:</strong>
                     <input type="text" name="company_name" value="" class="form-control" placeholder="Company Name">
                 </div>
             </div>
-            <div class="row">
-                <div class="col-xs-1 col-sm-1 col-md-1">
-                    <strong>Select:</strong>
-                </div>
-                <div class="col-xs-3 col-sm-3 col-md-3">
-                    <strong>Product Name:</strong>
-                </div>
-                <div class="col-xs-3 col-sm-3 col-md-3">
-                    <strong>Amount:</strong>
-                </div>
-                <div class="col-xs-3 col-sm-3 col-md-3">
-                    <strong>Quantity:</strong>
-                </div>
-            </div>
-            @foreach ($products as $value)
-            <div class="row">
-            <div class="col-xs-1 col-sm-1 col-md-1">
-                <div class="form-group">
-                    <input type="checkbox" value="{{$value->id}}" name="product_id[]">
-                </div>
-            </div>
-            <div class="col-xs-3 col-sm-3 col-md-3">
-                <div class="form-group">
-                    <input type="text" name="product_name[]" class="form-control" value="{{ $value->product_name }}" readonly>
-                </div>
-            </div>
-            <div class="col-xs-3 col-sm-3 col-md-3">
-                <div class="form-group">
-                    <input type="number" name="amount[]" class="form-control" value="{{ $value->amount }}" required>
-                </div>
-            </div>
-            <div class="col-xs-3 col-sm-3 col-md-3">
-                <div class="form-group">
-                    <input type="number" name="qty[]" class="form-control" value="0" required>
-                </div>
-            </div>
-            </div>
-            @endforeach
+
+            <table id="myTable" class=" table order-list">
+                <thead>
+                    <tr>
+                        <td>Product Name</td>
+                        <td>Rate</td>
+                        <td>Quantity</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="col-sm-4">
+                            <div class="form-group">
+                                <select class="form-select" aria-label="multiple select example" name="product_id[]" id="product_id1" data-id="1">
+                                    <option selected>Open this select menu</option>
+                                    @foreach($products as $val)
+                                    <option value="{{$val->id}}">{{$val->product_name}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </td>
+                        <td class="col-sm-4">
+                            <input type="number" name="rate[]" id="rate1" data-id="1" class="form-control rate"/>
+                        </td>
+                        <td class="col-sm-3">
+                            <input type="number" name="qty[]" id="qty1" data-id="1" class="form-control qty"/>
+                        </td>
+                        <td class="col-sm-2"><a class="deleteRow" data-id="1"></a>
+
+                        </td>
+                    </tr>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colspan="5" style="text-align: left;">
+                            <input type="button" class="btn btn-lg btn-block " id="addrow" value="Add Row" />
+                        </td>
+                    </tr>
+                    <tr>
+                    </tr>
+                </tfoot>
+            </table>
+
+
+            <input type="hidden" name="grandtotal" id="grandtotal" value="0" class="form-control"/>
+
 
             <div class="col-xs-12 col-sm-12 col-md-12" style="margin-top: 1%;">
               <button type="submit" class="btn btn-primary" style="float: right;">Submit</button>
@@ -85,3 +99,71 @@
    
 </form>
 @endsection
+<!-- <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"> -->
+<!-- <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script> -->
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<script type="text/javascript">
+    $(document).ready(function () {
+        var counter = 0;
+        $("#addrow").on("click", function () {
+        var rowCount = $(".rate").length+1;
+        alert(rowCount); 
+            var newRow = $("<tr>");
+            var cols = "";
+
+            cols += '<td class="col-sm-4"><div class="form-group"><select class="form-select" aria-label="multiple select example" name="product_id[]" id="product_id'+rowCount+'" data-id="'+rowCount+'"><option selected>Open this select menu</option>@foreach($products as $val)<option value="{{$val->id}}">{{$val->product_name}}</option>@endforeach</select></div></td>';
+            cols += '<td><input type="number" class="form-control rate" id="rate'+rowCount+'" data-id="'+rowCount+'" name="rate[]"/></td>';
+            cols += '<td><input type="number" class="form-control qty" id="qty'+rowCount+'" data-id="'+rowCount+'" name="qty[]"/></td>';
+
+            cols += '<td><input type="button" class="ibtnDel btn btn-md btn-danger " data-id="'+rowCount+'" value="Delete"></td>';
+            newRow.append(cols);
+            $("table.order-list").append(newRow);
+            counter++;
+        });
+
+
+
+        $("table.order-list").on("click", ".ibtnDel", function (event) {
+            $(this).closest("tr").remove();       
+            counter -= 1
+        });
+
+
+    });
+
+    $(document).ready(function(){
+
+            var sum=[];
+        $(document.body).on('change',".qty",function () {
+            var dataid = $(this).attr('data-id'); 
+            alert('dataid'+dataid);
+
+            var qnty = $(this).val();
+            var rate_val = $('#rate'+dataid).val();
+
+            var row_sum = parseInt(qnty)*parseFloat(rate_val);
+            
+            sum.push(row_sum);
+            console.log('sum',sum);
+            // for (var i in sum) {
+            //     console.log('rrr',sum[i]);
+            //   sum[i];
+            // }
+
+            // Creating variable to store the sum
+let sum1 = 0;
+ 
+// Running the for loop
+for (let i = 0; i < sum.length; i++) {
+    //console.log('rrr',sum[i]);
+     sum1 += sum[i];
+}
+ 
+            alert('total'+sum1);
+            // $('#grandtotal').val(total);
+
+        });
+    });
+
+
+</script>>
